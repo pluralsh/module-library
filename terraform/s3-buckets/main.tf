0cb850ce56877a00af26b5a2133ea156cfa27469
David van der Spek <vanderspek.david@gmail.com>
2023-01-25 13:17:19 +0100

    fix: don't use KMS
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- terraform/s3-buckets/main.tf
+++ terraform/s3-buckets/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+        sse_algorithm     = "AES256"
       }
     }
   }
