131c40179893d5f407742a5c7e432e9e3bc6f8c8
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-24 17:55:37 +0200

    refactor user_data module

--- terraform/eks-node-groups/multi-az-node-groups/modules/user_data/variables.tf
+++ terraform/eks-node-groups/multi-az-node-groups/modules/user_data/variables.tf
@@ -1,14 +1,3 @@
-variable "create" {
-  description = "Determines whether to create user-data or not"
-  type        = bool
-  default     = true
-}
-
-variable "platform" {
-  description = "Identifies if the OS platform is `bottlerocket`, `linux`, or `windows` based"
-  type        = string
-  default     = "linux"
-}
 
 variable "enable_bootstrap_user_data" {
   description = "Determines whether the bootstrap configurations are populated within the user data template"
@@ -17,7 +6,7 @@ variable "enable_bootstrap_user_data" {
 }
 
 variable "is_eks_managed_node_group" {
-  description = "Determines whether the user data is used on nodes in an EKS managed node group. Used to determine if user data will be appended or not"
+  description = "Determines whether the user data is used on nodes in an EKS managed node group. Always true for plural clusters (probably)."
   type        = bool
   default     = true
 }
@@ -47,19 +36,19 @@ variable "cluster_service_ipv4_cidr" {
 }
 
 variable "pre_bootstrap_user_data" {
-  description = "User data that is injected into the user data script ahead of the EKS bootstrap script. Not used when `platform` = `bottlerocket`"
+  description = "User data that is injected into the user data script ahead of the EKS bootstrap script."
   type        = string
   default     = ""
 }
 
 variable "post_bootstrap_user_data" {
-  description = "User data that is appended to the user data script after of the EKS bootstrap script. Not used when `platform` = `bottlerocket`"
+  description = "User data that is appended to the user data script after of the EKS bootstrap script."
   type        = string
   default     = ""
 }
 
 variable "bootstrap_extra_args" {
-  description = "Additional arguments passed to the bootstrap script. When `platform` = `bottlerocket`; these are additional [settings](https://github.com/bottlerocket-os/bottlerocket#settings) that are provided to the Bottlerocket user data"
+  description = "Additional arguments passed to the bootstrap script."
   type        = string
   default     = ""
 }
