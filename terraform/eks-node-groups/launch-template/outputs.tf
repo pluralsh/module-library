80762af3d521a60434e10bfd613898dc935c9b52
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-28 13:00:58 +0200

    add user data to output for debugging

--- terraform/eks-node-groups/launch-template/outputs.tf
+++ terraform/eks-node-groups/launch-template/outputs.tf
@@ -17,3 +17,8 @@ output "launch_template_name" {
   description = "The name of the launch template"
   value       = try(aws_launch_template.this.name, null)
 }
+
+output "user_data" {
+  description = "Base64 encoded user data rendered for the provided inputs"
+  value       = try(module.user_data.user_data, null)
+}
