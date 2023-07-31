f16bd3015500f95e1093fc932bdae15371dba8bf
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-31 18:50:50 +0200

    debug labels

--- terraform/eks-node-groups/launch-template/outputs.tf
+++ terraform/eks-node-groups/launch-template/outputs.tf
@@ -22,3 +22,19 @@ output "user_data" {
   description = "Base64 encoded user data rendered for the provided inputs"
   value       = try(module.user_data.user_data, null)
 }
+
+output "user_data_kubelet_extra_args" {
+  value = module.user_data.kubelet_extra_args
+}
+
+output "kubelet_extra_args" {
+  value = var.kubelet_extra_args
+}
+
+output "k8s_labels" {
+  value = var.k8s_labels
+}
+
+output "k8s_taints" {
+  value = var.k8s_taints
+}
