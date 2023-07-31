7240fba236f3ec876bd3196a730037626ae4236e
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-31 18:41:39 +0200

    debug kubelet args

--- terraform/eks-node-groups/user-data/main.tf
+++ terraform/eks-node-groups/user-data/main.tf
@@ -18,6 +18,7 @@ data "cloudinit_config" "linux_eks_managed_node_group" {
 }
 
 locals {
+  kubelet_extra_args = var.kubelet_extra_args != {} ? join(" ", [for k, v in var.kubelet_extra_args : "${k}=${v}"]) : ""
   int_linux_default_user_data = (var.enable_bootstrap_user_data || var.user_data_template_path != "") ? base64encode(templatefile(
     coalesce(var.user_data_template_path, "${path.module}/linux_user_data.tpl"),
     {
@@ -29,7 +30,7 @@ locals {
       cluster_auth_base64 = var.cluster_auth_base64
       # Optional
       cluster_service_ipv4_cidr = var.cluster_service_ipv4_cidr != null ? var.cluster_service_ipv4_cidr : ""
-      kubelet_extra_args        = var.kubelet_extra_args != {} ? join(" ", [for k, v in var.kubelet_extra_args : "${k}=${v}"]) : ""
+      kubelet_extra_args        = local.kubelet_extra_args
       bootstrap_extra_args      = var.bootstrap_extra_args
       pre_bootstrap_user_data   = var.pre_bootstrap_user_data
       post_bootstrap_user_data  = var.post_bootstrap_user_data
