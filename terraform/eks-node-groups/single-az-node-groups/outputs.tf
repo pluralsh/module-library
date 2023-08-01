a9838b787c4ba30f06dda540f7d9a0a85c2db32c
Hans Rauer <h.w.rauer@googlemail.com>
2023-08-01 12:59:15 +0200

    fix

--- terraform/eks-node-groups/single-az-node-groups/outputs.tf
+++ terraform/eks-node-groups/single-az-node-groups/outputs.tf
@@ -26,17 +26,17 @@ output "ng_expanded" {
 }
 
 output "user_data_kubelet_extra_args" {
-  value = module.launch_templates[*].user_data_kubelet_extra_args
+  value = values(module.launch_templates)[*].user_data_kubelet_extra_args
 }
 
 output "kubelet_extra_args" {
-  value = module.launch_templates[*].kubelet_extra_args
+  value = values(module.launch_templates)[*].kubelet_extra_args
 }
 
 output "k8s_labels" {
-  value = module.launch_templates[*].k8s_labels
+  value = values(module.launch_templates)[*].k8s_labels
 }
 
 output "k8s_taints" {
-  value = module.launch_templates[*].k8s_taints
+  value = values(module.launch_templates)[*].k8s_taints
 }
