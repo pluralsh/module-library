1f03e8a6476b575c50250a3b838d3fcae3e0e98a
Hans Rauer <h.w.rauer@googlemail.com>
2023-08-15 17:59:37 +0200

    clean up

--- terraform/eks-node-groups/user-data/main.tf
+++ terraform/eks-node-groups/user-data/main.tf
@@ -1,10 +1,3 @@
-# https://github.com/aws/containers-roadmap/issues/596#issuecomment-675097667
-# An important note is that user data must in MIME multi-part archive format,
-# as by default, EKS will merge the bootstrapping command required for nodes to join the
-# cluster with your user data. If you use a custom AMI in your launch template,
-# this merging will NOT happen and you are responsible for nodes joining the cluster.
-# See docs for more details -> https://docs.aws.amazon.com/eks/latest/userguide/launch-templates.html#launch-template-user-data
-
 data "cloudinit_config" "linux_eks_managed_node_group" {
   base64_encode = true
   gzip          = false
